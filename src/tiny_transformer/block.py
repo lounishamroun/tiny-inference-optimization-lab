4b53038c53e89d434e318cbfe19ef3416124f1fc
Lounis <lounis.hamroun@gmail.com>
2026-06-07 16:01:10 +0200

    feat: Add proper tokenizer for decoder block

--- src/tiny_transformer/block.py
+++ src/tiny_transformer/block.py
@@ -9,12 +9,12 @@ vocab_size =  8k–32k
 
 from . import data_loader  
 from tokenizers import Tokenizer
-from transformers import BertTokenizer
+from transformers import AutoTokenizer
 
 INPUT_TEXT = data_loader.return_text("data/text.txt")
 
 # We'll use a pre-trained tokenizer since we'll use quite generic data
-tokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
 tokenized_text=tokenizer.tokenize(INPUT_TEXT )
 print(tokenized_text)
 

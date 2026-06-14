e71f2509c0cd2291611a6d6b4afd1c68d9990a19
Lounis <lounis.hamroun@gmail.com>
2026-06-14 13:28:09 +0200

    feat: tokenizer turned into a function

---
+++ docs/plan.md
@@ -0,0 +1,2 @@
+
+text[1,5] => tokenizer
--- src/tiny_transformer/block.py
+++ src/tiny_transformer/block.py
@@ -7,18 +7,26 @@ context_length = 512
 vocab_size =  8k–32k
 """
 
+
+
 from . import data_loader  
 from tokenizers import Tokenizer
 from transformers import AutoTokenizer
 
 INPUT_TEXT = data_loader.return_text("data/text.txt")
 
+
+def tokenize_text(INPUT_TEXT):
 # We'll use a pre-trained tokenizer since we'll use quite generic data
-tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
-tokenized_text=tokenizer(INPUT_TEXT, return_tensors="pt")
-print(tokenized_text['input_ids'].shape)
+    tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
+    tokenized_text=tokenizer(INPUT_TEXT, return_tensors="pt") #tensor([[ 464, 3616,  286, 1204,  318]]) => Each word has been tokenized
+    return tokenized_text
 
 
-
-
-    
+""" 
+hidden_dim
+num_heads
+mlp_ratio
+dropout=0.0
+bias=True
+"""

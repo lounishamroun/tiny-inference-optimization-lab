e77ce44f53d8bd4dd9b16ba5481e5e4124a39c5e
Lounis <lounis.hamroun@gmail.com>
2026-06-19 16:33:11 +0200

    docs:Fixing glossary

--- src/tiny_transformer/block.py
+++ src/tiny_transformer/block.py
@@ -3,7 +3,7 @@ Decoder (GPT STYLE)
     Shapes:
         n_layers = 4 : Number of transformer blocks.
         d_model = 768 : Correponds to the length of our embeddings.
-        n_heads = 4
+        n_heads = 12
         head_dim = 64
         B = 1 : Batch size
         T = 5 : Sequence length (in this case 5 words)
@@ -27,6 +27,7 @@ INPUT_TEXT = data_loader.return_text("data/text.txt")
 tokenizer_config=PreTrainedConfig(output_hidden_states=True,output_attentions=True)
 model=AutoModel.from_pretrained("openai-community/gpt2")
 
+print(model.get_input_embeddings())
 
 def tokenize_text(INPUT_TEXT,tokenizer_config=None):
 # We'll use a pre-trained tokenizer since we'll use quite generic data
@@ -48,19 +49,22 @@ def ids_to_embeddings(token_ids):
         Returns => shape:[s,d_model] :
             `?`: Returns the list of embeddings corresponding to each of our sequence of tokens.
     """
+    token_embeddings=model.get_input_embeddings()
+    d_model=token_embeddings.embedding_dim
     T=len(token_ids)
-    
-    token_embedding_weights=next(model.named_parameters("wte"))[1]
-    token_embedding_obj=nn.Embedding.from_pretrained(token_embedding_weights)
+
     positional_weights=next(model.named_parameters("wpe"))[1]
     positional_embedding_obj=nn.Embedding.from_pretrained(positional_weights) 
 
-    
-    tok_embed=token_embedding_obj(token_ids) #shape=([T, d_model]) | type:Torch.Tensor
+    tok_embed=token_embeddings(token_ids) #shape=([T, d_model]) | type:Torch.Tensor
     pos_embed=positional_embedding_obj(torch.arange(start=0,end=T)) #shape=([T, d_model]) | type:Torch.Tensor
-    full_embed=tok_embed+pos_embed #shape=([T, d_model]) | type:Torch.Tensor
-       
-    return full_embed.unsqueeze(0)
+    full_embed=tok_embed+pos_embed #shape=([T, d_model]) | type:Torch.Tensor  
+    full_embed=full_embed.unsqueeze(0)
+    
+    assert full_embed.shape[-1] == d_model
+    assert full_embed.shape[0] == 1
+    return full_embed #TO DO : Dynamically adapt batch size
+
 
 
 token_ids_test=tokenize_text(INPUT_TEXT=INPUT_TEXT)

bf95672d969fa66bc26415fd1938566f470beb28
Lounis <lounis.hamroun@gmail.com>
2026-06-21 15:07:54 +0200

    fix: Add positional embedding conversion

--- src/tiny_transformer/block.py
+++ src/tiny_transformer/block.py
@@ -13,7 +13,7 @@ Decoder (GPT STYLE)
 
 from . import data_loader
 import torch
-from transformers import AutoTokenizer, PreTrainedConfig, AutoModel
+from transformers import AutoTokenizer, AutoModel
 
 
 if torch.cuda.is_available():
@@ -23,7 +23,7 @@ else:
 
 INPUT_TEXT = data_loader.return_text("data/text.txt")
 
-tokenizer_config=PreTrainedConfig()
+
 model=AutoModel.from_pretrained("openai-community/gpt2")
 model=model.to(DEVICE)
 
@@ -31,38 +31,49 @@ def tokenize_text(INPUT_TEXT):
 # We'll use a pre-trained tokenizer since we'll use quite generic data
     tokenizer = AutoTokenizer.from_pretrained(
         pretrained_model_name_or_path="openai-community/gpt2",
-        config_model_type=tokenizer_config
         )
-    token_ids=tokenizer(INPUT_TEXT, return_tensors="pt")['input_ids'] #=> Tokenizes each word
+    token_ids=tokenizer(INPUT_TEXT, return_tensors="pt")['input_ids'] #=> Converts text into token IDs.
     token_ids=token_ids.to(DEVICE)
     return token_ids #shape [B,T]
 
-def ids_to_embeddings(token_ids):
+def ids_to_gpt2_input_embeddings(token_ids):
     """
         Used to output a corresponding embedding given a token ID.
 
         Args:
-            word_ids => (`torch.Tensor`) | shape:[B,T,d_model] :
-                Token IDs corresponding to each tokens of our text sequence.
-            
+            word_ids =>  dtype: torch.Tensor | shape:[B, T] :
+                meaning: vocabulary indices
+                
         Returns:
             Type: `torch.Tensor` | shape:[B,T,d_model] :
                 List of embeddings corresponding to each of our sequence of tokens.
     """
-    embedding_obj=model.get_input_embeddings()
-    embedding_obj=embedding_obj.to(DEVICE)
-    d_model=embedding_obj.embedding_dim
-    T=len(token_ids[0]) 
-    final_embedding=embedding_obj(token_ids) #shape=([B,T, d_model]) | type:Torch.Tensor 
-    assert final_embedding.shape[-2] == T
-    assert final_embedding.shape[-1] == d_model
-    assert final_embedding.shape[0] == 1
-    return final_embedding #TO DO : Dynamically adapt batch size
-
+    B, T = token_ids.shape
+    token_embedding_module = model.get_input_embeddings()
+    position_embedding_module = model.wpe
+    
+    #### Token Embeddings ####
+    tok_embeddings=token_embedding_module(token_ids)
+    tok_embeddings=tok_embeddings.to(DEVICE)
+    print(f'tok embedding shape {tok_embeddings.shape}')
+    
+    #### Positional Embeddings ####
+    pos_embeddings=position_embedding_module(torch.arange(start=0,end=T).to(DEVICE))
+    pos_embeddings=pos_embeddings.to(DEVICE)
+    print(f'pos_embeddings shape {pos_embeddings.shape}')
+    
+    d_model=tok_embeddings.shape[-1]
+    print(f'd model {d_model}')
+    
+    #### Final Embedding ####
+    final_embedding=tok_embeddings+pos_embeddings  #shape=([B,T, d_model]) | type:Torch.Tensor 
+    print(f'final embedding {final_embedding.shape}')
+    assert final_embedding.shape == torch.Size([B, T, d_model]), f'Shape is {B, T, d_model}'
+    return final_embedding 
 
 token_ids_test=tokenize_text(INPUT_TEXT=INPUT_TEXT)
-embedding_for_seq=ids_to_embeddings(token_ids=token_ids_test)
-print(type(embedding_for_seq))
+embedding_for_seq=ids_to_gpt2_input_embeddings(token_ids=token_ids_test)
+print(embedding_for_seq)
 
 
 

7b3bfd7944103f85cbb21e8880f4732a5fc886e7
Lounis <lounis.hamroun@gmail.com>
2026-06-21 19:56:53 +0200

    Enhance assertions error text

--- boilerplates/similarity_test.py
+++ boilerplates/similarity_test.py
@@ -1,10 +1,10 @@
 import torch
 
 def compare_tensor_pair(t1,t2,allclose=True):
-    assert type(t1) == torch.Tensor, "Object not a tensor" 
-    assert type(t2) == torch.Tensor, "Object not a tensor"
-    assert t1.device == t2.device, "Different device between both tensors"
-    assert t1.shape == t2.shape, "Different shape between both tensors"
+    assert type(t1) == torch.Tensor, f"Object not a tensor {type(t1)}" 
+    assert type(t2) == torch.Tensor, f"Object not a tensor {type(t2)}"
+    assert t1.device == t2.device, f"Different device between both tensors {t1.device} VS {t2.device}"
+    assert t1.shape == t2.shape, f"Different shape between both tensors {t1.shape} VS {t2.shape}"
     if allclose == True:
-        assert torch.allclose(t1,t2), "Important value discrepancy between both tensors"
+        assert torch.allclose(t1,t2), f"Important value discrepancy between both tensors"
     
